47e418254780fb000c0c60c8bf20f2104c899639
Neil Campbell <neil@codecise.com>
2015-01-22 23:20:01 +1100

    Adding support for recording happiness button clicks

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 var five = require('johnny-five');
 var board = five.Board();
 
+var staffHappiness = [];
+staffHappiness.add = function(item) {
+	console.log(item.rating);
+	staffHappiness.push(item);
+}
+
+var greenButton; //good
+var yellowButton; //average
+var redButton; //bad
+
 board.on('ready', function() {
-	var led = new five.Led(13);
-	led.strobe();
+	greenButton = new five.Button({ pin: 2, invert: true });
+	yellowButton = new five.Button({ pin: 3, invert: true });
+	redButton = new five.Button({ pin: 5, invert: true });
+
+	greenButton.on('down', recordGoodRating);
+	yellowButton.on('down', recordAverageRating);
+	redButton.on('down', recordBadRating);
 });
+
+function recordGoodRating() {
+	staffHappiness.add({ rating: 'Good' });
+}
+
+function recordAverageRating() {
+	staffHappiness.add({ rating: 'Average' });
+} 
+
+function recordBadRating() {
+	staffHappiness.add({ rating: 'Bad' });
+}
